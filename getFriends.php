8ff9939f6cff6c4c02bf835c578702fa56a1ac1a
Fazle Rabbi <frduiit@gmail.com>
2017-04-26 01:06:15 +0600

    fr-25-1

---
+++ getFriends.php
@@ -0,0 +1,23 @@
+<?php
+	error_reporting(0); 
+	if (!empty($_POST))
+	{
+		$sqlConnect=mysqli_connect('localhost', 'root', '', 'mock');
+	
+		if(!$sqlConnect)
+		die("Error".mysqli_connect_errno());
+	
+		$id = 'rabbi';
+		$query = "SELECT id,name,latitude,longitude FROM Account,Friendship WHERE Account.id=Friendship.id1 and Friendship.id2='".$id."' and Friendship.status='accepted' or Account.id=Friendship.id2 and Friendship.id1='".$id."' and Friendship.status='accepted'";
+		$result = mysqli_query($sqlConnect, $query);
+		$rows = array();
+		
+		if(mysqli_num_rows($result)>0)
+		while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+		{
+			$rows[] = $row;
+		}
+		print json_encode($rows);
+		mysqli_close($sqlConnect);
+	}	
+?> 
