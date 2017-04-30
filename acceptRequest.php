01ee12f481a834edd3970febc619a170aac362da
Fazle Rabbi <frduiit@gmail.com>
2017-04-30 21:31:21 +0600

    fr-30-2

--- acceptRequest.php
+++ acceptRequest.php
@@ -7,10 +7,10 @@
 		if(!$sqlConnect)
 		die("Error".mysqli_connect_errno());
 	
-		$id1 = $_POST["id1"];
-		$id2 = $_POST["id2"];
+		$id1 = $_POST["id2"];
+		$id2 = $_POST["id1"];
 		
-		$query = "DELETE FROM Friendship WHERE id1='".$id1."' and id2='".$id2."'";
+		$query = "UPDATE Friendship SET status='accepted' WHERE id1='".$id1."' and id2='".$id2."'";
 		mysqli_query($sqlConnect, $query);
 		
 		mysqli_close($sqlConnect);
--- declineRequest.php
+++ declineRequest.php
@@ -7,8 +7,8 @@
 		if(!$sqlConnect)
 		die("Error".mysqli_connect_errno());
 	
-		$id1 = $_POST["id1"];
-		$id2 = $_POST["id2"];
+		$id1 = $_POST["id2"];
+		$id2 = $_POST["id1"];
 		
 		$query = "DELETE FROM Friendship WHERE id1='".$id1."' and id2='".$id2."'";
 		mysqli_query($sqlConnect, $query);
